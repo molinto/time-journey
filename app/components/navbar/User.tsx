365acdc6d29cb104a0d33476cd73a09bdfb93cf3
dragonflypie <paper-tiger@yandex.ru>
2023-05-01 14:27:15 +0400

    feat: implement user image

--- app/components/navbar/User.tsx
+++ app/components/navbar/User.tsx
@@ -1,9 +1,14 @@
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import UserIcon from "../icons/UserIcon";
 
 const User = () => {
+  const { data: session } = useSession();
+  const image = session?.user?.image;
+
   return (
-    <div className="hidden rounded-full border border-gray-600 p-1 md:block">
-      <UserIcon size={16} />
+    <div className="relative  hidden h-10 w-10 items-center justify-center overflow-hidden rounded-full border border-gray-600 p-1 md:flex">
+      {image ? <Image src={image} alt="avatar" fill /> : <UserIcon size={26} />}
     </div>
   );
 };
