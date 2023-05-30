86e9cedd25712df4854699cc37b6f194a09bc217
dragonflypie <paper-tiger@yandex.ru>
2023-05-30 22:25:45 +0400

    fix: replace signOut callback url with https://time-journey.vercel.app/

--- app/components/navbar/NavMenu.tsx
+++ app/components/navbar/NavMenu.tsx
@@ -32,7 +32,7 @@ const NavMenu = ({ isOpen }: NavMenuProps) => {
             <MenuItem
               title="Log Out"
               onClick={() => {
-                signOut({ callbackUrl: "http://localhost:3000/" });
+                signOut({ callbackUrl: "https://time-journey.vercel.app/" });
               }}
             />
           </>
