96b3c13287fdaecb89ca6b00e5349f5252bb7544
dragonflypie <paper-tiger@yandex.ru>
2023-05-31 13:29:40 +0400

    fix: move nextauth options to pages dir

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "../../components/utils/prismadb";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../auth/[...nextauth]/route(to-remove)";
 
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
